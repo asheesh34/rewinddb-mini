8a4a726a4120c602e686ace8084b55054a56e5ac
Asheesh Gautam <asheeshgautam428@gmail.com>
2026-07-17 11:15:03 +0530

    Initial project skeleton

---
+++ rewinddb-mini/backend/src/main/java/com/rewinddb/RewindDbApplication.java
@@ -0,0 +1,13 @@
+package com.rewinddb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewindDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewindDbApplication.class, args);
+    }
+
+}
